f2e50c6257de48799b4dfddbd3e6480d623cd1c3
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-02-06 00:47:13 -0300

    att

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 
 import './App.css';
 import Header from './common/header/Header';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 
 function App() {
   return (
     <div >
+      <Router>
       <Header />
+      <Switch>
+        {/* <Route path='/'>
+          <About />
+        </Route> */}
+      </Switch>
+      </Router>
     </div>
   );
 }
--- src/common/header/Navbar.jsx
+++ src/common/header/Navbar.jsx
@@ -1,16 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+
+  const [MobileMenu, setMobileMenu] = useState(false)
   return (
     <>
-        <header className="header">
-          <div className="container d-flex">
-            <div className="categoria d-flex">
-              <span className="fa-solid fa-border-all"></span>
-              <i class="fa-solid fa-border-all"></i>
-            </div>
+      <header className="header">
+        <div className="container d-flex">
+          <div className="categoria d-flex">
+            <span className="fa-solid fa-border-all"></span>
+            <h4>
+              Categorias <i className="fa-solid fa-chevron-down"></i>
+            </h4>
           </div>
-        </header>
+
+          <div className="navlink">
+            <ul className="nav">
+              <li>
+                <Link to='/'>home</Link>
+              </li>
+              <li>
+                <Link to='/pages'>paginas</Link>
+              </li>
+              <li>
+                <Link to='/user'>conta usuario</Link>
+              </li>
+              <li>
+                <Link to='/marcas'>conta marcas</Link>
+              </li>
+              <li>
+                <Link to='/localizar'>localize meu pedido</Link>
+              </li>
+              <li>
+                <Link to='/contato'>contato</Link>
+              </li>
+            </ul>
+
+            <button className="toggle" onClick={() => setMobileMenu(!MobileMenu)}>
+              {
+                MobileMenu? <i className="fas fa-times close home-btn"></i> : 
+                <i className="fas fa-bars open"></i>
+              }
+            </button>
+          </div>
+        </div>
+      </header>
     </>
   )
 }
